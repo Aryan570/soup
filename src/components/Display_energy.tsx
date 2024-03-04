914d46214c46d6b2288b1db0dc2276397287e2a9
Aryan <malhotraaryan570@gmail.com>
2024-03-04 11:50:05 +0530

    Present

--- src/components/Display_energy.tsx
+++ src/components/Display_energy.tsx
@@ -4,8 +4,12 @@ import React, { BaseSyntheticEvent, Suspense, useState } from 'react'
 
 const Display_energy = (props: any) => {
     const [Cost, setCost] = useState(10);
+    const [Budget,setBudget] = useState(10);
     function handleChange(e: BaseSyntheticEvent) {
         setCost(e.target.value)
+    }
+    function handleBudget(e : BaseSyntheticEvent){
+        setBudget((Cost/e.target.value)*100);
     }
     return (
         <Suspense fallback={<Loader2 />}>
@@ -35,11 +39,40 @@ const Display_energy = (props: any) => {
                         </div>
                     </div>
                 </div>
+                <div>
+                    <p className='pl-1 text-xs font-bold'>Enter Budget (Rs)</p>
+                    <div>
+                        <input type='number' defaultValue={Budget} onChange={handleBudget} min={10} id='budget' name='budget' className='rounded-lg p-2 border border-emerald-400 bg-transparent focus:ring-0 focus:outline-none focus:border-emerald-300 mr-2' required/>
+                    </div>
+                </div>
                 <div className=' bg-emerald-400 bg-contain rounded-lg text-white p-2 font-bold'>Carbon Footprint : {props.pwr.energy ? (0.85 * (props.pwr.energy/(3600*1000))).toFixed(3) : 0} Kgs of Carbon</div>
+                {/* <div className=' font-bold flex items-center border p-2 w-fit border-emerald-400 rounded-lg'>
+                    Condition : 
+                    <div className={`w-4 h-4 ${parseInt(props.pwr.current) > 240 ? 'bg-red-400' : 'bg-emerald-400'} mx-2 rounded-lg`} color='red'></div>
+                </div>
                 <div className=' font-bold flex items-center border p-2 w-fit border-emerald-400 rounded-lg'>
                     Condition : 
-                    <div className={`w-4 h-4 ${parseInt(props.pwr.voltage) > 240 ? 'bg-red-400' : 'bg-emerald-400'} mx-2 rounded-lg`} color='red'></div>
+                    <div className={`w-4 h-4 ${parseInt(props.pwr.current) > 240 ? 'bg-red-400' : 'bg-emerald-400'} mx-2 rounded-lg`} color='red'></div>
                 </div>
+                <div className=' font-bold flex items-center border p-2 w-fit border-emerald-400 rounded-lg'>
+                    Condition : 
+                    <div className={`w-4 h-4 ${parseInt(props.pwr.current) > 240 ? 'bg-red-400' : 'bg-emerald-400'} mx-2 rounded-lg`} color='red'></div>
+                </div> */}
+                <div className='font-bold pl-1 text-xs'>Alerts</div>
+                <table className='border border-spacing-4 border-separate rounded-lg border-emerald-400 font-mono text-center'>
+                    <tr className='text-xs'>
+                        <th>Voltage</th>
+                        <th>Current</th>
+                        <th>Budget</th>
+                    </tr>
+                    <tr>
+                        <td>{parseInt(props.pwr.voltage) > 220 || parseInt(props.pwr.voltage) <150 ? props.pwr.volatge > 240 ? <div className='text-rose-500 p-1 rounded-lg text-xs font-bold'>High</div> : <div className='text-rose-500 p-1 rounded-lg text-xs font-bold'>Low</div> : <div className='p-1 rounded-lg text-emerald-400 text-xs font-bold'>Normal</div>}</td>
+                        <td>{parseInt(props.pwr.current) > 5 ? <div className='text-rose-500 p-1 rounded-lg text-xs font-bold'>Abnormal</div> : <div className=' p-1 rounded-lg text-emerald-400 text-xs font-bold'>Normal</div>}</td>
+                        <td>{Budget > 80 ? <div className='text-xs p-1 rounded-lg font-bold text-rose-500'>{Budget.toFixed(3)}%</div> : <div className='text-xs p-1 rounded-lg font-bold text-emerald-400'>{Budget.toFixed(3)}%</div>}</td>
+                    </tr>
+                </table>
+                {Budget > 80 && Budget <100 ? <div className='text-xs rounded-lg text-amber-400 font-bold'>About to react the Budget limit</div> : <div></div>}
+                {Budget >= 100? <div className='text-xs rounded-lg text-rose-500 font-bold'>Budget limit has been crossed!</div> : <div></div>}
             </div>
         </Suspense>
     )
